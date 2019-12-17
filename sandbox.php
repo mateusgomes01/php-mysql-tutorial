8b53696ff774ed78987b7d594c25543bb087473d
Mateus Gomes <52260710+mateusgomes01@users.noreply.github.com>
2019-12-17 20:48:23 -0300

    session variables
    added some session variables manipulation

--- sandbox.php
+++ sandbox.php
@@ -1,7 +1,32 @@
  <?php
 
  	// ternary operators
+ /*
  	$score = 20;
+ 	echo $score > 40 ? 'high score!' : 'low score :(';
+
+ 	// superglobals = allways $_CAPITAL LETTERS
+
+ 	// $_GET['name'], $_POST['name']
+
+ 	echo $_SERVER['SERVER_NAME'] . '<br />';
+ 	echo $_SERVER['REQUEST_METHOD'] . '<br />';
+ 	echo $_SERVER['SCRIPT_FILENAME'] . '<br />';
+ 	echo $_SERVER['PHP_SELF'] . '<br />';
+ 	*/
+
+ 	// $_SESSION, $_COOKIE <= FUTURE SUPERGLOBALS
+
+ 	// session variables: stores information on server while running
+
+ 	if(isset($_POST['submit'])){
+
+ 		session_start(); //starts a session
+
+ 		$_SESSION['name'] = $_POST['name'];
+
+ 		header('location: index.php');
+ 	}
 
  ?>
 
@@ -12,7 +37,10 @@
  </head>
  <body>
 
- 	<p><?php echo $score > 40 ? 'high score!' : 'low score :(';?></p>
+ 	<form action="<?php echo $_SERVER['PHP_SELF'] ?>" method="POST">
+ 		<input type="text" name="name">
+ 		<input type="submit" name="submit" value="submit">
+ 	</form>
  
  </body>
  </html>
--- templates/header.php
+++ templates/header.php
@@ -1,3 +1,20 @@
+ <?php 
+
+  session_start();
+
+  //$_SESSION['name'] = 'Yoshi';
+
+  if($_SERVER['QUERY_STRING'] == 'noname'){
+    //unset($_SESSION['name']);
+    session_unset(); //clear all variables in $_SESSION
+  }
+
+  $name = $_SESSION['name'];
+
+
+
+  ?>
+
  <head>
  	<title>Ninja Pizza</title>
  	<!-- Compiled and minified CSS -->
@@ -29,6 +46,7 @@
   		<div class="container">
   			<a href="index.php" class="brand-logo brand-text center">Ninja Pizza</a>
   			<ul id="nav-mobile" class="right hide-on-small-and-down">
+          <li class="grey-text">Hello <?php echo htmlspecialchars($name) ?></li>
   				<li><a href="add.php" class="btn brand z-depth-1">Add a Pizza</a></li>
   			</ul>
   		</div>
